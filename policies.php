1cd54b46a54e84bf1444ca16bbf9372af518fe69
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2026-01-04 19:11:07 +0530

    changes

--- policies.php
+++ policies.php
@@ -7,7 +7,7 @@
     <title>GetwebUP - Best Web Hosting Company in India</title>
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- Google Fonts - Inter for a modern, professional look -->
+    <!-- Google Fonts: Inter -->
     <!--<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">-->
     <!-- Font Awesome for Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
